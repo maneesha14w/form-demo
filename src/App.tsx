7e5c1723a4dbc621e2b7feab2276dd38fa12d2b3
Maneesha <maneesha14w@gmail.com>
2024-04-06 21:47:42 +0530

    feat: added shadcn card with inputs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { CardWithForm } from "./components/CardComponent"
+import { ThemeProvider } from "./components/theme-provider"
 
-export default App;
+const App = () => {
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<div className="mx-auto flex h-screen w-full items-center justify-center">
+				<CardWithForm></CardWithForm>
+			</div>
+		</ThemeProvider>
+	)
+}
+
+export default App
---
+++ src/components/CardComponent.tsx
@@ -0,0 +1,41 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function CardWithForm() {
+	return (
+		<Card className="w-[350px]">
+			<CardHeader>
+				<CardTitle>Data Collector</CardTitle>
+				<CardDescription>Collecting your data since 2001</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<form>
+					<div className="grid w-full items-center gap-4">
+						<div className="flex flex-col space-y-1.5">
+							<Label htmlFor="name">Name</Label>
+							<Input id="name" placeholder="Your Name" />
+						</div>
+						<div className="flex flex-col space-y-1.5">
+							<Label htmlFor="district">District</Label>
+							<Input id="district" placeholder="District" />
+						</div>
+					</div>
+				</form>
+			</CardContent>
+			<CardFooter className="flex justify-end">
+				<Button>Submit</Button>
+			</CardFooter>
+		</Card>
+	)
+}
